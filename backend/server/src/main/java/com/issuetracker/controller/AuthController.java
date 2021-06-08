b84e4717e4b103e92a1547f0aa932c854f9527c5
Pyro(고정완) <rhwjddhks@gmail.com>
2021-06-08 17:01:37 +0900

    fix: UserArgumentResolver 적용

--- backend/server/src/main/java/com/issuetracker/controller/AuthController.java
+++ backend/server/src/main/java/com/issuetracker/controller/AuthController.java
@@ -1,16 +1,18 @@
 package com.issuetracker.controller;
 
 import com.issuetracker.annotation.LoginRequired;
-import com.issuetracker.dto.*;
-import com.issuetracker.dto.auth.*;
+import com.issuetracker.annotation.UserAttribute;
+import com.issuetracker.dto.MessageResponse;
+import com.issuetracker.dto.auth.AccessTokenResponse;
+import com.issuetracker.dto.auth.AuthRequest;
+import com.issuetracker.dto.auth.AuthResponse;
+import com.issuetracker.dto.auth.UserDto;
 import com.issuetracker.service.AuthService;
 import com.issuetracker.service.GitHubService;
 import com.issuetracker.service.UserService;
 import com.issuetracker.util.JwtUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -29,10 +31,9 @@ public class AuthController {
 
     @GetMapping("/hello")
     @LoginRequired
-    public MessageResponse getHello(HttpServletRequest request) {
-        UserDto userDto = (UserDto) request.getAttribute("user");
-        authService.authenticate(userDto);
-        return new MessageResponse("안녕하세요! 로그인 한 유저는 언제나 환영합니다!");
+    public MessageResponse getHello(@UserAttribute UserDto user) {
+        authService.authenticate(user);
+        return new MessageResponse("안녕하세요, " + user.getName() + " 님!\n로그인 한 유저는 언제나 환영합니다!");
     }
 
     @PostMapping("/auth")
