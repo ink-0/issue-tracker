6b9e51f7e115a2cca81d0287f9b416e821b2fd2a
Pyro(고정완) <rhwjddhks@gmail.com>
2021-06-17 11:55:44 +0900

    feat: DB 테이블의 CREATE 쿼리 초안 작성
    아래의 위키 문서를 참고하여 작성을 했다.
    Repository 에 하드 코딩되어 작성된 예시들을 Insert 해야한다.
    
    https://github.com/ghojeong/issue-tracker/wiki/%EB%8F%84%EB%A9%94%EC%9D%B8-%EB%8B%A4%EC%9D%B4%EC%96%B4%EA%B7%B8%EB%9E%A8

--- backend/docker/db/initdb/1_schema.sql
+++ backend/docker/db/initdb/1_schema.sql
@@ -1,8 +1,62 @@
 ALTER DATABASE `pyrodb` DEFAULT CHARACTER SET = utf8mb4;
-DROP TABLE IF EXISTS `pyrodb`.`user` ;
 
+DROP TABLE IF EXISTS `pyrodb`.`user`;
 CREATE TABLE `pyrodb`.`user` (
-    email VARCHAR(50) NOT NULL PRIMARY KEY,
-    name VARCHAR(50),
-    avatarUrl VARCHAR(300)
+    `email` VARCHAR(50) NOT NULL PRIMARY KEY,
+    `name` VARCHAR(50),
+    `avatarUrl` VARCHAR(300)
+);
+
+DROP TABLE IF EXISTS `pyrodb`.`status`;
+CREATE TABLE `pyrodb`.`status` (
+    `id` VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS `pyrodb`.`milestone`;
+CREATE TABLE `pyrodb`.`milestone` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(50) NOT NULL,
+    `description` VARCHAR(300),
+    `dueDate` TIMESTAMP
+);
+
+DROP TABLE IF EXISTS `pyrodb`.`issue`;
+CREATE TABLE `pyrodb`.`issue` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(50) NOT NULL,
+    `content` VARCHAR(300),
+    `writerId` VARCHAR(300) NOT NULL,
+    `statusId` VARCHAR(50) NOT NULL,
+    `milestoneId` INT,
+    CONSTRAINT `issue_writer_foreignkey` FOREIGN KEY(`writerId`) REFERENCES `pyrodb`.`user`(`email`),
+    CONSTRAINT `issue_status_foreignkey` FOREIGN KEY(`statusId`) REFERENCES `pyrodb`.`status`(`id`),
+    CONSTRAINT `issue_milestone_foreignkey` FOREIGN KEY(`milestoneId`) REFERENCES `pyrodb`.`milestone`(`id`)
+);
+
+DROP TABLE IF EXISTS `pyrodb`.`comment`;
+CREATE TABLE `pyrodb`.`comment` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `content` VARCHAR(300) NOT NULL,
+    `dateTime` TIMESTAMP,
+    `writerId` VARCHAR(300) NOT NULL,
+    `issueId` INT,
+    CONSTRAINT `comment_writer_foreignkey` FOREIGN KEY(`writerId`) REFERENCES `pyrodb`.`user`(`email`),
+    CONSTRAINT `comment_issue_foreignkey` FOREIGN KEY(`issueId`) REFERENCES `pyrodb`.`issue`(`id`)
+);
+
+DROP TABLE IF EXISTS `pyrodb`.`label`;
+CREATE TABLE `pyrodb`.`label` (
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(50) NOT NULL,
+    `description` VARCHAR(300),
+    `backgroundColor` VARCHAR(8)
+);
+
+DROP TABLE IF EXISTS `pyrodb`.`issueLabel`;
+CREATE TABLE `pyrodb`.`issueLabel` (
+    `issueId` INT,
+    `labelId` INT,
+    CONSTRAINT `issueLabel_issue_foreignkey` FOREIGN KEY(`issueId`) REFERENCES `pyrodb`.`issue`(`id`),
+    CONSTRAINT `issueLabel_label_foreignkey` FOREIGN KEY(`labelId`) REFERENCES `pyrodb`.`label`(`id`),
+    CONSTRAINT `issueLabel_primarykey` PRIMARY KEY(`issueId`, `labelId`)
 );
