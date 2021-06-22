dc28b477aac4f893ef7f61af15e75969a484fb5c
Pyro(고정완) <rhwjddhks@gmail.com>
2021-06-22 19:07:22 +0900

    refactor: IssueOptionDto 의 데이터 반환 형태를 간략화

--- backend/server/src/main/java/com/issuetracker/dto/ios/IosIssueOptionDto.java
+++ backend/server/src/main/java/com/issuetracker/dto/ios/IosIssueOptionDto.java
@@ -1,47 +1,38 @@
 package com.issuetracker.dto.ios;
 
 import com.issuetracker.domain.IssueOption;
+import com.issuetracker.dto.auth.UserDto;
+
+import java.util.List;
 
 //INFO, 새로운 이슈 작성할 때, [담당자, 레이블, 마일스톤] 보여주는 건대, 네이밍 변경이 필요해보임.
 public class IosIssueOptionDto {
 
-    private IosAssigneesDto assigneesDto;
+    private final IosAssigneesDto assignees;
 
-    private IosLabelsDto labelsDto;
+    private final IosLabelsDto labels;
 
-    private IosMilestonesDto milestonesDto;
+    private final IosMilestonesDto milestones;
 
     public static IosIssueOptionDto from(IssueOption issueOption) {
         return new IosIssueOptionDto(IosAssigneesDto.from(issueOption.getAssignees()), IosLabelsDto.from(issueOption.getLabels()), IosMilestonesDto.from(issueOption.getMilestones()));
     }
 
-    public IosIssueOptionDto(IosAssigneesDto assigneesDto, IosLabelsDto labelsDto, IosMilestonesDto milestonesDto) {
-        this.assigneesDto = assigneesDto;
-        this.labelsDto = labelsDto;
-        this.milestonesDto = milestonesDto;
+    public IosIssueOptionDto(IosAssigneesDto assignees, IosLabelsDto labels, IosMilestonesDto milestones) {
+        this.assignees = assignees;
+        this.labels = labels;
+        this.milestones = milestones;
     }
 
-    public IosAssigneesDto getAssigneesDto() {
-        return assigneesDto;
+    public List<UserDto> getAssignees() {
+        return assignees.getUsers();
     }
 
-    public IosLabelsDto getLabelsDto() {
-        return labelsDto;
+    public List<IosLabelDto> getLabels() {
+        return labels.getLabels();
     }
 
-    public void setAssigneesDto(IosAssigneesDto assigneesDto) {
-        this.assigneesDto = assigneesDto;
-    }
-
-    public void setLabelsDto(IosLabelsDto labelsDto) {
-        this.labelsDto = labelsDto;
-    }
-
-    public IosMilestonesDto getMilestonesDto() {
-        return milestonesDto;
-    }
-
-    public void setMilestonesDto(IosMilestonesDto milestonesDto) {
-        this.milestonesDto = milestonesDto;
+    public List<IosMilestoneDto> getMilestones() {
+        return milestones.getMilestones();
     }
 }
--- backend/server/src/main/java/com/issuetracker/dto/web/WebIssueOptionDto.java
+++ backend/server/src/main/java/com/issuetracker/dto/web/WebIssueOptionDto.java
@@ -1,35 +1,38 @@
 package com.issuetracker.dto.web;
 
 import com.issuetracker.domain.IssueOption;
+import com.issuetracker.dto.auth.UserDto;
+
+import java.util.List;
 
 //INFO, 새로운 이슈 작성할 때, [담당자, 레이블, 마일스톤] 보여주는 건대, 네이밍 변경이 필요해보임.
 public class WebIssueOptionDto {
 
-    private WebAssigneesDto assigneesDto;
+    private final WebAssigneesDto assignees;
 
-    private WebLabelsDto labelsDto;
+    private final WebLabelsDto labels;
 
-    private WebMilestonesDto milestonesDto;
+    private final WebMilestonesDto milestones;
 
     public static WebIssueOptionDto from(IssueOption issueOption) {
         return new WebIssueOptionDto(WebAssigneesDto.from(issueOption.getAssignees()), WebLabelsDto.from(issueOption.getLabels()), WebMilestonesDto.from(issueOption.getMilestones()));
     }
 
-    public WebIssueOptionDto(WebAssigneesDto assigneesDto, WebLabelsDto labelsDto, WebMilestonesDto milestonesDto) {
-        this.assigneesDto = assigneesDto;
-        this.labelsDto = labelsDto;
-        this.milestonesDto = milestonesDto;
+    public WebIssueOptionDto(WebAssigneesDto assignees, WebLabelsDto labels, WebMilestonesDto milestones) {
+        this.assignees = assignees;
+        this.labels = labels;
+        this.milestones = milestones;
     }
 
-    public WebAssigneesDto getAssigneesDto() {
-        return assigneesDto;
+    public List<UserDto> getAssignees() {
+        return assignees.getUsers();
     }
 
-    public WebLabelsDto getLabelsDto() {
-        return labelsDto;
+    public List<WebLabelDto> getLabels() {
+        return labels.getLabels();
     }
 
-    public WebMilestonesDto getMilestonesDto() {
-        return milestonesDto;
+    public List<WebMilestoneDto> getMilestones() {
+        return milestones.getMilestones();
     }
 }
